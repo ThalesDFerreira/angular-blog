2b3eedaa9822f04c25518bc8b5f1bd6e3e55fe0a
ThalesDFerreira <thalesferreira190@gmail.com>
2023-09-22 17:14:06 -0300

    feat: estruturando menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MmenuTitleComponent } from './components/mmenu-title/mmenu-title.component';
-import { MbigCardComponent } from './components/mbig-card/mbig-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
-import { SbigCardComponent } from './components/sbig-card/sbig-card.component';
-import { SigCardComponent } from './components/sig-card/sig-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 
@@ -16,11 +12,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
   declarations: [
     AppComponent,
     MenuBarComponent,
-    MmenuTitleComponent,
-    MbigCardComponent,
     SmallCardComponent,
-    SbigCardComponent,
-    SigCardComponent,
     MenuTitleComponent,
     BigCardComponent
   ],
