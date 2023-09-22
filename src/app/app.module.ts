0a1b07ac7836360ff28dc61dd7acab02b3fdc90f
ThalesDFerreira <thalesferreira190@gmail.com>
2023-09-22 16:57:14 -0300

    feat: criando os componentes da minha aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MmenuTitleComponent } from './components/mmenu-title/mmenu-title.component';
+import { MbigCardComponent } from './components/mbig-card/mbig-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { SbigCardComponent } from './components/sbig-card/sbig-card.component';
+import { SigCardComponent } from './components/sig-card/sig-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MmenuTitleComponent,
+    MbigCardComponent,
+    SmallCardComponent,
+    SbigCardComponent,
+    SigCardComponent,
+    MenuTitleComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule,
